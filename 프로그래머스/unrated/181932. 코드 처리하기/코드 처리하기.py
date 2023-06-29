8a2395036dc045d8bb022697028977c50d959084
yoekjang2 <137030764+yoekjang2@users.noreply.github.com>
2023-06-29 17:35:15 +0900

    [unrated] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -1,8 +1,7 @@
 def solution(code):
     mode = 0
     ret = ""
-    index = 0
-    for i in code:
+    for index, i in enumerate(code):
         if i == '1' and mode == 0:
             mode = 1
         elif i == '1' and mode == 1:
@@ -12,7 +11,6 @@ def solution(code):
                 ret += i
             elif mode == 1 and index%2 !=0:
                 ret += i
-        index += 1
     if ret == "":
         ret = "EMPTY"
     return ret
